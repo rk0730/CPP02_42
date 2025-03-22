5c55338d5b6b7bf2cd53dc70c1f986a9b45ec392
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2025-03-22 18:19:29 +0900

    ex02にex01を複製

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,66 @@
+#include "Fixed.hpp"
+
+const int Fixed::fractional_bits = 8;
+
+Fixed::Fixed() : value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int value)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->value = value << fractional_bits;
+}
+
+Fixed::Fixed(const float value)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->value = roundf(value * (1 << fractional_bits));
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	operator=(other);
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+		value = other.getRawBits();
+	return *this;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits() const
+{
+	return value;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	value = raw;
+}
+
+float Fixed::toFloat() const
+{
+	return (float)value / (1 << fractional_bits);
+}
+
+int Fixed::toInt() const
+{
+	return value >> fractional_bits;
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed)
+{
+	(void)fixed;
+	out << fixed.toFloat();
+	return out;
+}
---
+++ ex02/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+public:
+	Fixed();
+	Fixed(const int value);
+	Fixed(const float value);
+	Fixed(const Fixed &other);
+	Fixed &operator=(const Fixed &other);
+	~Fixed();
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat(void) const;
+	int toInt(void) const;
+	friend std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
+
+private:
+	int value;
+	static const int fractional_bits;
+};
+
+#endif
