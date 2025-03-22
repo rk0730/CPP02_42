68fd9b171702f4390830814c029ab3453c149a50
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2025-03-22 18:13:15 +0900

    ex01

--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -2,16 +2,22 @@
 #define FIXED_HPP
 
 #include <iostream>
+#include <cmath>
 
 class Fixed
 {
 public:
 	Fixed();
+	Fixed(const int value);
+	Fixed(const float value);
 	Fixed(const Fixed &other);
 	Fixed &operator=(const Fixed &other);
 	~Fixed();
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
+	float toFloat(void) const;
+	int toInt(void) const;
+	friend std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
 
 private:
 	int value;
