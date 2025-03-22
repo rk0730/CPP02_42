25643e0020c41e26916d49d75b83f2df2f13115c
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2025-03-22 18:30:31 +0900

    コンストラクタ、デストラクタ出力削除

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -3,40 +3,32 @@
 const int Fixed::fractional_bits = 8;
 
 Fixed::Fixed() : value(0)
-{
-	std::cout << "Default constructor called" << std::endl;
-}
+{}
 
 Fixed::Fixed(const int value)
 {
-	std::cout << "Int constructor called" << std::endl;
 	this->value = value << fractional_bits;
 }
 
 Fixed::Fixed(const float value)
 {
-	std::cout << "Float constructor called" << std::endl;
 	this->value = roundf(value * (1 << fractional_bits));
 }
 
 Fixed::Fixed(const Fixed &other)
 {
-	std::cout << "Copy constructor called" << std::endl;
 	operator=(other);
 }
 
 Fixed &Fixed::operator=(const Fixed &other)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
 		value = other.getRawBits();
 	return *this;
 }
 
 Fixed::~Fixed()
-{
-	std::cout << "Destructor called" << std::endl;
-}
+{}
 
 int Fixed::getRawBits() const
 {
