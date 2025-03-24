4080bf4aa349fd3308da745515bf9084e1639666
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2025-03-24 18:57:51 +0900

    ex03

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,53 +1,19 @@
-#include "Fixed.hpp"
+#include "Point.hpp"
+
+bool is_point_in_triangle(const Point p, const Point a, const Point b, const Point c);
 
 int main(void)
 {
-	Fixed a(10);
-	Fixed b(3.3f);
+	Point a(Fixed(0), Fixed(0));
+	Point b(Fixed(10), Fixed(0));
+	Point c(Fixed(0), Fixed(10));
 
-	if (a < b)
-		std::cout << "a is smaller than b" << std::endl;
-	if (a > b)
-		std::cout << "a is greater than b" << std::endl;
-	if (a <= b)
-		std::cout << "a is smaller or equal to b" << std::endl;
-	if (a >= b)
-		std::cout << "a is greater or equal to b" << std::endl;
-	if (a == b)
-		std::cout << "a is equal to b" << std::endl;
-	if (a != b)
-		std::cout << "a is not equal to b" << std::endl;
+	Point p(Fixed(15), Fixed(15));
 
-	std::cout << "a + b = " << a + b << std::endl;
-	std::cout << "a - b = " << a - b << std::endl;
-	std::cout << "a * b = " << a * b << std::endl;
-	std::cout << "a / b = " << a / b << std::endl;
-
-	Fixed plus = a + b;
-	std::cout << "a + b = " << plus << std::endl;
-
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << --a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a-- << std::endl;
-	std::cout << a << std::endl;
-
-	Fixed inc = a++;
-	std::cout << "a++ = " << inc << std::endl;
-
-	Fixed smaller = Fixed::min(a, b);
-	std::cout << "Smaller: " << smaller << std::endl;
-	Fixed greater = Fixed::max(a, b);
-	std::cout << "Greater: " << greater << std::endl;
-
-	Fixed const c(42);
-	Fixed const d(21);
-	std::cout << "Smaller: " << Fixed::min(c, d) << std::endl;
-	std::cout << "Greater: " << Fixed::max(c, d) << std::endl;
+	if (is_point_in_triangle(a, b, c, p))
+		std::cout << "Point p is inside the triangle" << std::endl;
+	else
+		std::cout << "Point p is outside the triangle" << std::endl;
 
 	return 0;
 }
